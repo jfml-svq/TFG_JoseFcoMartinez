fcb467aa50930686a72e240735c879dc6ee407fd
Jose Fco Martinez <79310869+jfml-svq@users.noreply.github.com>
2023-11-10 06:51:02 +0100

    Iniciando proyecto

---
+++ src/main/java/com/josefco/camping/campingsvq/CampingSvqApplication.java
@@ -0,0 +1,13 @@
+package com.josefco.camping.campingsvq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingSvqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampingSvqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josefco/camping/campingsvq/CampingSvqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josefco.camping.campingsvq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingSvqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
